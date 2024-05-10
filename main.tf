ee518869d76d4539dd8bab008521278a175649a7
elicarter <89507487+elicarter@users.noreply.github.com>
2024-05-10 09:12:18 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
 provider "aws" {
   region = "us-west-2"
 }
+
+resource "aws_instance" "AcmeDemoServer" {
+  count         = var.instance_count
+  ami           = "ami-830c94e3"
+  instance_type = var.aws_instance_type
+
+  tags = {
+    Name = "AcmeWebServer-${count.index + 1}"
+
+    }
+}
