90b3668c87a43e3d9f6b1acc788606b668c732ee
elicarter <89507487+elicarter@users.noreply.github.com>
2024-05-10 09:13:09 -0600

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,3 +2,9 @@ variable "aws_instance_type" {
   description = "Instance type for AWS EC2 instance"
   default     = "t2.micro"
 }
+
+variable "instance_count" {
+  description = "Number of instances to create"
+  type        = number
+  default     = 1
+}
